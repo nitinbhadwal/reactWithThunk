fa655254b5db8e5becc10bdc14fe82a3653331d1
Nitin_Bhadwal <nitin.bhadwal@blazeclan.com>
2021-08-03 12:36:52 +0530

    added thunk middleware

--- src/Redux/Actions/productAction.js
+++ src/Redux/Actions/productAction.js
@@ -1,17 +1,14 @@
 import { ActionTypes } from "../Constants/action-types";
 import axios from "axios";
 
-export const setProducts = async (dispatch) => {
-  const response = await axios
-    .get("https://fakestoreapi.com/products")
-    .catch((error) => {
-      console.log(error);
+export const setProducts = () => {
+  return async function (dispatch) {
+    const response = await axios.get("https://fakestoreapi.com/products");
+    dispatch({
+      type: ActionTypes.SET_PRODUCTS,
+      payload: response.data,
     });
-
-  dispatch({
-    type: ActionTypes.SET_PRODUCTS,
-    payload: response.data,
-  });
+  };
 };
 
 export const selectedProducts = (products) => {
